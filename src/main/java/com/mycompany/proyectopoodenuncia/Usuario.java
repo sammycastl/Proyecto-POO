4d0fe83b15948264bdd70293c0af68e838f1f759
sammycastl <nsmartinezcastl@gmail.com>
2024-03-28 11:34:01 -0600

    Añadir rol en clase Usuario

--- src/main/java/com/mycompany/proyectopoodenuncia/Usuario.java
+++ src/main/java/com/mycompany/proyectopoodenuncia/Usuario.java
@@ -4,10 +4,12 @@ package com.mycompany.proyectopoodenuncia;
 public class Usuario {
     private String nombre;
     private String contraseña;
+    private Rol rol;
 
-    public Usuario(String nombre, String contraseña) {
+    public Usuario(String nombre, String contraseña, Rol rol) {
         this.nombre = nombre;
         this.contraseña = contraseña;
+        this.rol = rol;
     }
 
     // Método para validar las credenciales del usuario
@@ -18,6 +20,25 @@ public class Usuario {
     // Método para cambiar la contraseña del usuario
     public void cambiarContraseña(String nuevaContraseña) {
         this.contraseña = nuevaContraseña;
+    }
+    
+    // Metodo para obtener el rol del usuario
+    public Rol getRol(){
+        return rol;
+    }
+     // Método para establecer el rol del usuario
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    // Método para comprobar si el usuario tiene permiso de administrador
+    public boolean esAdmin() {
+        return this.rol == Rol.ADMINISTRADOR;
+    }
+
+    // Método para comprobar si el usuario tiene permiso de crear denuncias
+    public boolean puedeCrearDenuncias() {
+        return this.rol == Rol.ADMINISTRADOR || this.rol == Rol.USUARIO_NORMAL;
     }
 
     // Getters y setters
@@ -36,4 +57,9 @@ public class Usuario {
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
+}
+
+enum Rol {
+    ADMINISTRADOR,
+    USUARIO_NORMAL
 }
